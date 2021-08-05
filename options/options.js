4842f2dd66990f91757e098e872b442599c336f7
Crowdbotics <team@crowdbotics.com>
2021-08-05 10:46:03 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "babymorphosa_29503Identifier",
+  url: "https://babymorphosa_29503Identifier.botics.co",
+  api: "https://babymorphosa_29503Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
